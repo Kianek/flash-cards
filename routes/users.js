5bc49b521561ac298067c92f3c011272bcaa4f2e
Keanu Allridge <kallridge92@gmail.com>
2018-12-23 20:57:43 -0800

    Add register functionality

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,37 @@
 const errors = require('restify-errors');
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
+const usersUrl = '/api/users';
 
 module.exports = server => {
   // Register Account
-  server.get('/', (req, res, next) => {
-    res.send('test');
-    next();
+  server.post(`${usersUrl}/register`, async (req, res, next) => {
+    const { name, email, password, security } = req.body;
+
+    // Check whether this user is already registered
+    User.findOne({ email }, (err, user) => {
+      if (user) {
+        res.json({ msg: 'That email is already registered' });
+        next();
+      }
+
+      // If not registered, create new user, hash password, and save
+      const newUser = new User({ name, email, password, security });
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, async (err, hash) => {
+          // Hash Password
+          newUser.password = hash;
+
+          try {
+            await newUser.save();
+            res.send(201);
+            next();
+          } catch (err) {
+            return next(new errors.InternalError(err));
+          }
+        });
+      });
+    });
   });
 
   // Edit Account
