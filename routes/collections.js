8674a953984ea966e301f93ba5e725e34733b88c
Keanu Allridge <kallridge92@gmail.com>
2018-12-26 07:08:47 -0800

    Add route to get all of a user's collections

--- routes/collections.js
+++ routes/collections.js
@@ -1,11 +1,40 @@
 const errors = require('restify-errors');
+const rjwt = require('restify-jwt-community');
+const config = require('../config');
 const bcrypt = require('bcryptjs');
 const auth = require('../auth');
-const Collection = require('../models/Collection');
+const User = require('../models/User');
+const collectionsUrl = '/api/collections';
 
 module.exports = server => {
-  // server.get('/', (req, res, next) => {
-  //   res.send('test');
-  //   next();
-  // });
+  // server.get(
+  //   collectionsUrl,
+  //   rjwt({ secret: config.JWT_SECRET }),
+  //   async (req, res, next) => {
+  //     res.send('Collections test');
+  //     return next();
+  //   }
+  // );
+
+  // Get all collections for the user
+  server.get(
+    collectionsUrl,
+    rjwt({ secret: config.JWT_SECRET }),
+    async (req, res, next) => {
+      try {
+        const result = await User.findById(req.user._id);
+
+        if (result) {
+          res.send(result.collections);
+          return next();
+        }
+      } catch (err) {
+        return next(new errors.NotFoundError(err));
+      }
+    }
+  );
+
+  // Get Single Collection
+
+  // Delete Single Collection
 };
